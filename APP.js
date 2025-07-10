6c7e30405c3c6c2c60274d9b0ad5b54045ce93f5
wartech2 <163756489+wartech2@users.noreply.github.com>
2025-07-10 23:11:44 +0530

    a

--- APP.js
+++ APP.js
@@ -18,6 +18,10 @@ import ReactDOM from "react-dom/client"
 // JSX => React.createElement => ReactElement-JS Object =>  HTMLElement(render)
 const jsxHeading = <h1 className="head">React using Shauryaa🚀</h1>;
 
+// React Component
+// Class Based Components - OLD way
+// Function Based Components - NEW way
+
 console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

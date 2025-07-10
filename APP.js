f7c5f46baac17170656c433333febe4b29f242d4
wartech2 <163756489+wartech2@users.noreply.github.com>
2025-07-10 20:51:51 +0530

    file

--- APP.js
+++ APP.js
@@ -14,12 +14,15 @@
  * ReactElement(Object) => HTMl(Browser Understand)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement(
      "div", 
      {id: "parent"},
      [React.createElement("div", {id: "child"}, [
-     React.createElement("h1", {}, "I'm an h1 tag"),
-     React.createElement("h2", {}, "I'm an h2 tag"),
+     React.createElement("h1", {}, "React 🚀"),
+     React.createElement("h2", {}, "by Shaurya Pratap Singh"),
      ]),
 
      React.createElement("div", {id: "child2"}, [
